3fbd9ddbdd776ad07fb371e7c9e343cf8a03de7d
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-26 14:45:21 +0300

    feat: initial commit for install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Installiere die Abhängigkeiten..."
+
+# Beispielhafte Abhängigkeiten installieren
+sudo apt-get update
+sudo apt-get install -y git
+# Fügen Sie weitere Installationsbefehle hier hinzu
+
+echo "Installation abgeschlossen!"
